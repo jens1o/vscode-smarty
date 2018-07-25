0d97b3c21badce5ba4abad538f8bdad2371587c7
jens1o <jenshausdorf@gmail.com>
2018-07-25 19:18:45 +0200

    started with Modifier-Completion

---
+++ src/extension.ts
@@ -0,0 +1,37 @@
+import * as vscode from 'vscode';
+import CompletionProvider from './completionProvider';
+
+let completionProvider: vscode.Disposable;
+
+function getTriggerCharacters(): string[] {
+    const delimiter = vscode.workspace.getConfiguration('smarty.delimiter');
+
+    return [delimiter.left, '|'];
+}
+
+function registerCompletionProvider(): void {
+    completionProvider = vscode.languages.registerCompletionItemProvider(
+        [
+            { language: 'smarty', scheme: 'file' },
+            { language: 'smarty', scheme: 'untitled' },
+        ],
+        new CompletionProvider(),
+        ...getTriggerCharacters()
+    );
+
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('The Smarty extension is active!');
+
+    vscode.workspace.onDidChangeConfiguration(e => {
+        if (e.affectsConfiguration('smarty.delimiter') && completionProvider !== null) {
+            completionProvider.dispose();
+            completionProvider = null;
+            registerCompletionProvider();
+        }
+    })
+    registerCompletionProvider();
+
+    context.subscriptions.push(completionProvider);
+}
